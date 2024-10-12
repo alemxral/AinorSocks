78a73ef41e6abc0d60f60037d5bfc767a43694b2
alemxral <alejandromoralcontact@gmail.com>
2024-10-12 14:26:14 +0200

    Initial commit with project files

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -20,8 +20,8 @@ export default async function handler(req, res) {
                     },
                 ],
                 mode: 'payment',
-                success_url: `${req.headers.origin}/success`,
-                cancel_url: `${req.headers.origin}/cancel`,
+                success_url: `${req.headers.origin}/success.html`,
+                cancel_url: `${req.headers.origin}/cancel.html`,
             });
 
             res.status(200).json({ id: session.id });
